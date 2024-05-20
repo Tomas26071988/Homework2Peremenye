c53dd1c1905ff8cf4ecb9403eccc035d3394e053
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-05-20 14:56:49 +0300

    Add files via upload

---
+++ Homework2Peremennay.py
@@ -0,0 +1,7 @@
+Amount_of_homeworks = 12
+print(type(Amount_of_homeworks))
+Amount_of_hours = float(1.5)
+name = "Python"
+result = (Amount_of_hours / Amount_of_homeworks)
+print(result)
+print("Курс:", name, "Всего задач:", Amount_of_homeworks, "Затрачено часов:", Amount_of_hours, "время вып-ния", result)
---
+++ Homework2ZdachaVsem.py
@@ -0,0 +1,8 @@
+print('Введите количество (кг)')
+kg = float(4)
+print('Введите цену')
+price = float(34)
+print("Введите сумму платежа")
+summa = float(1000)
+ch = (summa - (price * kg))
+print("Сумма сдачи", ch)
